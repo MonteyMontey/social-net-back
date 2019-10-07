ecf4592b46e18f763e4b58a4ec377f6e87284192
MonteyMontey <32668472+MonteyMontey@users.noreply.github.com>
2019-10-07 09:01:51 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("Index page");
+});
+
+const port = 5000;
+
+app.listen(port,() => {
+  console.log(`Server started on port ${port}`);
+});
