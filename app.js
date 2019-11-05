768ded6656631ad17cb6fd95e29cff8067a7436f
monteymontey <dominik.klein@code.berlin>
2019-11-05 10:26:31 +0100

    add registration model and create endpoint for registraiton

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+// Connect to mongoose
+mongoose.connect('mongodb+srv://montey:montey@freetiercluster-wg6nd.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected successfully!'))
+.catch(err => console.log(err));
+
+// Load model
+require('./models/Registration');
+const Registration = mongoose.model('registration');
+
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+app.post('/registration', (req, res) => {
+  console.log("Successfully received registration data: ", req.body);
+  res.status(200).send();
+});
+
 
 app.get('/', (req, res) => {
   res.send("Index page");
 });
+
 
 const port = 5000;
 
