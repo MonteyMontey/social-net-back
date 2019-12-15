0416426b250a134509893bbe701491bc6e2d05f6
monteymontey <dominik.klein@code.berlin>
2019-12-15 15:13:52 +0100

    add winston logger

---
+++ app/helpers/logger.js
@@ -0,0 +1,21 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info', // log only if info.level less than or equal to this level
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+});
+
+// also log to console when not in production
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
+}
+
+
+module.exports = logger;
--- app/routes/login.js
+++ app/routes/login.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 
+const logger = require('../helpers/logger');
+
 dotenv.config();
 
 // Load models
@@ -18,18 +20,27 @@ router.post('/', (req, res) => {
     if (user && err === null) {
     user.comparePassword(password, (err, isMatch) => {
       if (isMatch && err === null) {
-        console.log("valid login", user);
         const id = user._id;
         const payload = { id };
         const token = jwt.sign(payload, process.env.SECRET, { expiresIn: '1d' });
         res.cookie('token', token).sendStatus(200);
+        logger.info('User logged in', {
+          user: user,
+          date: new Date()
+        });
       } else {
-        console.log("invalid login");
+        logger.warn('Invalid password', {
+          email: email,
+          date: new Date()
+        });
         res.status(401).send();
       };
     })
     } else {
-      console.log("user not found")
+      logger.warn('User login not found', {
+        email: email,
+        date: new Date()
+      })
       res.status(401).send();
     }
   })
